1d400cc5b66b546da97fe8fa6de906bf86046651
nicolas42 <nicolas42@gmail.com>
2019-11-16 11:44:06 +1000

    find

--- find.go
+++ find.go
@@ -5,19 +5,23 @@ import (
 	"os"
 	"path/filepath"
 	"strings"
+
+	"github.com/atotto/clipboard"
 )
 
 func visit(path string, f os.FileInfo, err error) error {
 
+	// print(".")
 	containsAll := true
-	for i, _ := range searchTerms {
-		if !strings.Contains(f.Name(), searchTerms[i]) {
+	for i := range searchTerms {
+		if !strings.Contains(strings.ToLower(f.Name()), strings.ToLower(searchTerms[i])) {
 			containsAll = false
 			break
 		}
 	}
 	if containsAll {
 		fmt.Println(path)
+		clipboard.WriteAll(path)
 	}
 
 	return nil
@@ -30,6 +34,7 @@ func main() {
 
 	if len(os.Args) < 3 {
 		fmt.Printf("usage: %s [dir] [search terms]\r\n", os.Args[0])
+		return
 	}
 
 	root = os.Args[1]
